387a76b8f85e7e4b19c323e44c74f691423250f7
Josh Allan <allan.josh07@gmail.com>
2020-11-23 10:09:11 -0500

    Tests failed.

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const session = require('express-session');
-require('dotenv').config();
 const routes = require('./routes/html-routes.js');
 const passport = require('./config/passport');
 const exphbs = require('express-handlebars');
